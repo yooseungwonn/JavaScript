1623791910cb44638cc5290c0a4c73732511b0f2
yooseungwonn <163090539+yooseungwonn@users.noreply.github.com>
2024-05-28 16:32:36 +0900

    my birthday

--- 05-flow-control.js
+++ 05-flow-control.js
@@ -61,6 +61,22 @@ while(a<5){
 }
 console.log(star);
 
+// for ... in : 객체의 속성 키를 반복
+const obj = {
+  name: "홍길동",
+  age: 28,
+  job: "도적"
+}
+
+for (let key in obj){
+  console.log(`${key} -> ${obj[key]}`);
+}
+
+// for ... of : 배열을 순회
+const arr = [1, 2, 3, 4, 5];
+for (let item of arr){
+  console.log(item);
+} 
   
   
   
---
+++ ex02-js/script/ask.js
@@ -0,0 +1,1 @@
+let name = prompt("이름을 입력해주세요");
